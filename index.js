fddd1e9ed5a5f1aaa40805bcb0b0333e113a10ec
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2020-11-02 13:38:54 +0300

    final

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ log(chalk.green(names.name2));
 log(chalk.yellow(names.name3));
 log(chalk.blue(names.name4));
 
+//МАРК НАПИСАЛ ТАК:
+
+let students = [
+    {
+      name: 'Aya',
+      color: 'blue'
+    },
+    {
+      name: 'Jane',
+      color: 'red'
+    },
+    {
+      name: 'Jacopo',
+      color: 'yellow'
+    },
+    {
+      name: 'Ioan',
+      color: 'green'
+    },
+  ]
+  for(let i =0; i < students.length; i++){
+    console.log(chalk[students[i].color](students[i].name))
+  }
+  
